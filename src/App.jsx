64d5e60d98d4850b0529f88d6ff50d2fbb9b9ffd
KateLotero <katerine.lotero@correounivalle.edu.co>
2023-07-30 18:16:32 -0500

    se agregó evento cuando se da click en el botón de seguir

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center  justify-center h-screen space-y-4">
-        <TwitterFollowCard />
-        <TwitterFollowCard />
+        <TwitterFollowCard
+          userName="KateLotero"
+          name="Katerine Lotero"
+          avatar="https://random-d.uk/api/248.jpg"
+          initialIsFollowing
+        />
+        <TwitterFollowCard
+          userName="Gatito"
+          name="Wilson Machado"
+          avatar="https://imagenes.20minutos.es/files/og_thumbnail_1900/uploads/imagenes/2022/11/10/gato-naranja-sam-chang-unsplash.jpeg"
+        />
       </div>
     </>
   );
--- src/components/TwitterFollowCard.jsx
+++ src/components/TwitterFollowCard.jsx
@@ -1,26 +1,35 @@
-function TwitterFollowCard() {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+function TwitterFollowCard({ userName, name, avatar, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+
+  const handelClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <div className="flex items-center space-x-4">
       <article>
         <header className="flex items-center space-x-4">
-          <img
-            className="w-12 h-12 rounded-full"
-            alt="El avatar midudev"
-            src="https://random-d.uk/api/248.jpg"
-          />
+          <img className="w-12 h-12 rounded-full" alt="avatar" src={avatar} />
           <div>
-            <strong>Katerine Lotero</strong>
-            <div>@kateLotero</div>
+            <strong>{name}</strong>
+            <div>@{userName}</div>
           </div>
         </header>
       </article>
       <aside>
-        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Seguir
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={handelClick}
+        >
+          {text}
         </button>
       </aside>
     </div>
   );
 }
-
 export default TwitterFollowCard;
